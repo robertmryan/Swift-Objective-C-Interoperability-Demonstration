243902168d74ff9ea30b45252dffe2cc6dd8a7cb
Robert Ryan <rryan@Robs-MBP.home>
2015-01-09 16:18:48 -0500

    Initial Commit

---
+++ Swift-Objective-C-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift-Objective-C-Test
+//
+//  Created by Robert Ryan on 1/9/15.
+//  Copyright (c) 2015 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Swift-Objective-C-Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Swift-Objective-C-Test
+//
+//  Created by Robert Ryan on 1/9/15.
+//  Copyright (c) 2015 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
